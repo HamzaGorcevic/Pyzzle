aca40de8c490eb542b765b2f2f9931824ac7c3a5
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-11-25 17:13:32 +0100

    backup server

--- pyzzle_backend/pyzzle_backend/settings.py
+++ pyzzle_backend/pyzzle_backend/settings.py
@@ -146,7 +146,9 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'https://pyzzle.onrender.com',
-    'https://pyzzlebackend.onrender.com'
+    'https://pyzzlebackend.onrender.com',
+    'https://pyzzlebackend1.onrender.com'
+
 ]
 
 CORS_ALLOW_METHODS = [
