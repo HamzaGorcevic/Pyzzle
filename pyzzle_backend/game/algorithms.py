02e3966381b138829bd0b9f6ac32c82a395d5683
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-11-26 11:23:32 +0100

    working with tuples instead of matrixes

--- pyzzle_backend/game/algorithms.py
+++ pyzzle_backend/game/algorithms.py
@@ -9,126 +9,145 @@ class Algorithm(ABC):
     def get_steps(self, initial_state, goal_state):
         pass
     @staticmethod
-    def possible_paths(current_state):
-        row_pos=0
-        col_pos=0
-        for i in range(len(current_state)):
-            for j in range(len(current_state[0])):
-                if(current_state[i][j]==0):
-                    row_pos=i
-                    col_pos=j
-        
-        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+    def possible_paths(current_state: tuple[int, ...], size: int = 3):
+        zero_index = current_state.index(0)
+        row_pos, col_pos = divmod(zero_index, size)
+
+        # Define possible move directions (right, down, up, left)
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         paths = []
-        for dir in directions:
-            move_row = row_pos + dir[0]
-            move_col = col_pos + dir[1]
-            if 0 <= move_row < len(current_state) and  0<= move_col < len(current_state[0]):
-                new_state =  [row[:] for row in current_state]     
-                new_state[row_pos][col_pos],new_state[move_row][move_col] = new_state[move_row][move_col],new_state[row_pos][col_pos]
-                action = move_row * len(current_state[0]) + move_col
-                paths.append((new_state, action))
+
+        for dir_row, dir_col in directions:
+            move_row = row_pos + dir_row
+            move_col = col_pos + dir_col
+
+            if 0 <= move_row < size and 0 <= move_col < size:
+                new_index = move_row * size + move_col
+
+                new_state = list(current_state)  
+                new_state[zero_index], new_state[new_index] = new_state[new_index], new_state[zero_index]
+                new_state = tuple(new_state)  
+
+                paths.append((new_state, new_index))
+
         return paths
+
     @staticmethod
-    def id_linearization(state):
-        return tuple(num for row in state for num in row)
+    def manhattan_distance(state, goal_state):
+        heuristic_sum = 0
+        size = int(len(state) ** 0.5)  # Assuming the state is a square grid (e.g., 3x3 for the 8-puzzle)
         
-    # HEURISTIKE
-    @staticmethod
-    def manhatn_distance(state,goal_state):
-        heuristic_sum =0
-        for i in range(len(state)):
-            for j in range(len(state[0])):
-                value = state[i][j]
-                if value !=0:
-                    # this will return row as target_i, and modul will be column target_j
-                    target_i,target_j = divmod(value,len(state))
-                    heuristic_sum+= abs(i-target_i) + abs(j-target_j)
-        return heuristic_sum
-    @staticmethod
-    #checking for those wich are in their place
-    def haming(state,goal_state):
-        heuristic_sum =0
-        for i in range(len(state)):
-            for j in range(len(state[0])):
-                if state[i][j]!=goal_state[i][j] and  state[i][j] != 0:
-                    heuristic_sum+=1
+        for index, value in enumerate(state):
+            if value != 0:  # Ignore the empty space (0)
+                target_index = goal_state.index(value)
+                target_i, target_j = divmod(target_index, size)
+                current_i, current_j = divmod(index, size)
+                heuristic_sum += abs(current_i - target_i) + abs(current_j - target_j)
+
         return heuristic_sum
     
+    @staticmethod
+    def hamming(state, goal_state):
+        heuristic_sum = 0
+        for index, value in enumerate(state):
+            if value != 0 and value != goal_state[index]:
+                heuristic_sum += 1
+        return heuristic_sum
+
+
     def heuristic(self, state, goal_state):
         if self.distance == "manhattan":
-            return Algorithm.manhatn_distance(state, goal_state)
+            return Algorithm.manhattan_distance(state, goal_state)
         elif self.distance == "hamming":
-            return Algorithm.haming(state, goal_state)
+            return Algorithm.hamming(state, goal_state)
         else:
             raise ValueError("Unknown heuristic method.")
 
 
         
 class BFSAlgorithm(Algorithm):
-    def get_steps(self, initial_state, goal_state):
-        queue = deque([(initial_state,[])])
-        visited = set()
-        visited.add(tuple(tuple(row) for row in initial_state))
-        nodes_explored =0
+    def get_steps(self, initial_state: tuple[int, ...], goal_state: tuple[int, ...]):
+        queue = deque([(initial_state, [])])
+        visited = {initial_state}  # Add the initial state as a tuple of tuples
+        nodes_explored = 0
 
         while queue:
             state, path = queue.popleft()
-            nodes_explored+=1
+            nodes_explored += 1
+
             if state == goal_state:
-                return (path,nodes_explored)
-            for new_state, action in Algorithm.possible_paths(state):
-                tuple_state = tuple(tuple(row) for row in new_state)
-                if tuple_state not in visited:
+                return (path, nodes_explored)
+
+            for new_state, action in self.possible_paths(state):
+                if new_state not in visited:  # Check if the state has already been visited
                     queue.append((new_state, path + [action]))
-                    visited.add(tuple_state)
+                    visited.add(new_state)  # Add the new state to the visited set
+
         return None
 
 
 
 class BestFSAlgorithm(Algorithm):
-    def get_steps(self, initial_state, goal_state):
+    def get_steps(self, initial_state: tuple[int, ...], goal_state: tuple[int, ...]):
+        # Priority queue with tuples of (heuristic value, unique ID, state, path)
         queue = []
-        initial_h = self.heuristic(initial_state,goal_state)
-        heapq.heappush(queue, (initial_h,Algorithm.id_linearization(initial_state), initial_state, []))
-        visited = set()
-        visited.add(tuple(tuple(row) for row in initial_state))
-        nodes_explored = 0
-        while queue:
-            h,id, current_state, path = heapq.heappop(queue)
-            nodes_explored +=1
-            if current_state == goal_state:
-                return (path,nodes_explored)
-            for new_state, action in Algorithm.possible_paths(current_state):
-                tuple_state = tuple(tuple(row) for row in new_state)
-                if tuple_state not in visited:
-                    new_h = self.heuristic(new_state,goal_state)
-                    heapq.heappush(queue, (new_h, Algorithm.id_linearization(new_state),new_state, path + [action]))
-                    visited.add(tuple_state)
-        return None
-
-
-class   AStarAlgorithm(Algorithm):
-    def get_steps(self, initial_state, goal_state):
-        queue = []
-        initial_h = self.heuristic(initial_state,goal_state)
-        heapq.heappush(queue, (initial_h,Algorithm.id_linearization(initial_state), 0, initial_state, []))
-        visited = set()
-        visited.add(tuple(tuple(row) for row in initial_state))
-        nodes_explored =0
-
-        while queue:
-            f, id,g, current_state, path = heapq.heappop(queue)
-            nodes_explored +=1
-            if current_state == goal_state:
-                return (path,nodes_explored)
-            for new_state, action in Algorithm.possible_paths(current_state):
-                tuple_state = tuple(tuple(row) for row in new_state)
-                if tuple_state not in visited:
-                    new_g = g + 1
-                    new_h = self.heuristic(new_state,goal_state)
-                    new_f = new_g + new_h 
-                    heapq.heappush(queue, (new_f, Algorithm.id_linearization(initial_state),new_g, new_state, path + [action]))
-                    visited.add(tuple_state)
+        initial_h = self.heuristic(initial_state, goal_state)
+        heapq.heappush(queue, (initial_h, initial_state, []))
         
-        return None
+        visited = set()
+        visited.add(initial_state)  # Add the initial state as a tuple
+        
+        nodes_explored = 0
+
+        while queue:
+            h, current_state, path = heapq.heappop(queue)
+            nodes_explored += 1
+
+            if current_state == goal_state:
+                return (path, nodes_explored)
+
+            # Explore possible paths from the current state
+            for new_state, action in self.possible_paths(current_state):
+                if new_state not in visited:
+                    new_h = self.heuristic(new_state, goal_state)
+                    heapq.heappush(queue, (new_h,new_state, path + [action]))
+                    visited.add(new_state)  # Add the new state to the visited set
+
+        return None  # Return None if no solution is found
+
+import heapq
+
+class AStarAlgorithm(Algorithm):
+    def get_steps(self, initial_state, goal_state):
+        queue = []
+        
+        # Use heuristic to calculate the initial 'f' value (f = g + h)
+        initial_h = self.heuristic(initial_state, goal_state)
+        heapq.heappush(queue, (initial_h, 0, initial_state, []))  # (f, g, state, path)
+        
+        visited = set()
+        visited.add(initial_state)  # Add the initial state as a flat tuple
+        nodes_explored = 0
+        
+        while queue:
+            f, g, current_state, path = heapq.heappop(queue)
+            nodes_explored += 1
+
+            # If the current state matches the goal, return the solution path and nodes explored
+            if current_state == goal_state:
+                return (path, nodes_explored)
+            
+            # Generate possible next states
+            for new_state, action in Algorithm.possible_paths(current_state):
+                if new_state not in visited:
+                    visited.add(new_state)  # Mark the state as visited
+                    
+                    # Calculate g (cost to reach the state) and h (heuristic from the state to goal)
+                    new_g = g + 1
+                    new_h = self.heuristic(new_state, goal_state)
+                    new_f = new_g + new_h  # f = g + h
+                    
+                    # Push the new state onto the priority queue
+                    heapq.heappush(queue, (new_f, new_g, new_state, path + [action]))
+        
+        return None  # If no solution is found
--- pyzzle_backend/game/views.py
+++ pyzzle_backend/game/views.py
@@ -7,7 +7,7 @@ from .algorithms import *
 @csrf_exempt
 def start_game(request, algorithm, heuristic=None):
     if request.method == "POST":
-        goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        goal_state = tuple([1,2,3,4,5,6,7,8,0])
         body = loads(request.body)
         if algorithm == "BFS":
             algo_instance = BFSAlgorithm()
@@ -17,8 +17,8 @@ def start_game(request, algorithm, heuristic=None):
             algo_instance = AStarAlgorithm(heuristic)
         else:
             return JsonResponse({"error": "Invalid algorithm."}, status=400)
-        
-        response, nodes_explored = algo_instance.get_steps(body.get("initial_state"), goal_state)
+        start_state = tuple(item for row in body.get("initial_state") for item in row)
+        response, nodes_explored = algo_instance.get_steps(start_state, goal_state)
         return JsonResponse({"steps": response, "nodes_explored": nodes_explored})
     else:
         return JsonResponse({"error": "Invalid request method. Use POST."}, status=400)
